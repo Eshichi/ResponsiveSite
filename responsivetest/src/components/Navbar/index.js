12975c88441d528cd1cabe4e1b95628e0ca3ae41
Eshichi <envy_nil@yahoo.com>
2020-12-11 18:27:55 +0800

    collapse created

--- responsivetest/src/components/Navbar/index.js
+++ responsivetest/src/components/Navbar/index.js
@@ -1,13 +1,28 @@
 import React, { Component} from 'react';
-import {Row, Col } from 'antd';
+import {Row, Col, Button } from 'antd';
 import './Navbar.css';
 import Logo from '../../Images/logo.png'
+import { MenuFoldOutlined } from '@ant-design/icons';
 
 class Navbar extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            isDropDownVisible: "link-hidden"
+        }
     }
 
+    handleDropDown = () =>{
+        if(this.state.isDropDownVisible === "link-hidden"){
+            this.setState({
+                isDropDownVisible: "link-show"
+            });
+        }else{
+            this.setState({
+                isDropDownVisible: "link-hidden"
+            });
+        }
+    }
     render(){
         return(
             <div className="navbar-container">
@@ -46,6 +61,30 @@ class Navbar extends Component {
                                     </div>
                                 </Col>
                             </Row>
+                        </div>
+                        <div className="mobile-navbar-section">
+                            <Row>
+                                <Col xs={24}>
+                                    <div className="mobile-navbar-content">
+                                        <Row>
+                                            <Col xs={24}>
+                                                <div className="mobile-navbar-image">
+                                                        <img src={Logo} />
+                                                        <Button onClick={this.handleDropDown}><MenuFoldOutlined /></Button>
+                                                </div>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                        <div className={`${this.state.isDropDownVisible}`}> 
+                            <span>Site Links</span>
+                            <Button>Home</Button>
+                            <Button>About Us</Button>
+                            <Button>Insight</Button>
+                            <Button>Events</Button>
+                            <Button>Contact Us</Button>
                         </div>
                     </Col>
                </Row>
