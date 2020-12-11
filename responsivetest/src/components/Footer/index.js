605a93696546f7c1d1a7fc0478ec035ca467c854
Eshichi <envy_nil@yahoo.com>
2020-12-11 17:48:17 +0800

    created footer

--- responsivetest/src/components/Footer/index.js
+++ responsivetest/src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react';
 import {Row, Col } from 'antd';
 import './Footer.css';
+import {  TwitterOutlined, FacebookOutlined , LinkedinOutlined, MailOutlined} from '@ant-design/icons';
 
 class Footer extends Component {
     constructor(props){
@@ -11,13 +12,23 @@ class Footer extends Component {
         return(
             <div className="footer-container">
                 <Row>
-                    <Col>
+                    <Col xs={24}>
                         <div className="footer-section">
                             <Row>
-                                <Col>
-                                    <div className="footer-section">
-                                        
-                                    </div>
+                                <Col xs={12}>
+                                        <div className="footer-contact">
+                                            <span>Call us at 1111-222-333</span>
+                                            <span>for more information</span>
+                                        </div>
+                                </Col>
+                                <Col xs={12}>
+                                        <div className="footer-social">
+                                            <span>Social Share</span>
+                                            <TwitterOutlined />
+                                            <FacebookOutlined />
+                                            <LinkedinOutlined />
+                                            <MailOutlined />
+                                        </div>
                                 </Col>
                             </Row>
                         </div>
